6524b5f22a8e8f391e6832ae814152b1c12f7e28
StrowBeary <remicaillot5@gmail.com>
2018-11-06 14:15:20 +0100

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './stylesheets/App.css';
+import {TestComponent} from "./components/testcomponent/testcomponent";
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
           >
             Learn React
           </a>
+            <TestComponent/>
         </header>
       </div>
     );
---
+++ src/components/testcomponent/testcomponent.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './_testcomponent.scss';
+
+export class TestComponent extends Component {
+    render () {
+        return (
+            <div className="testcomponent">
+
+            </div>
+        );
+    }
+}
