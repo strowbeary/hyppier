e8a5420bef9934a5e13b5fa68e4e195e236780b3
mel <melanie.ngo@edu.gobelins.fr>
2018-11-27 13:16:27 +0100

    Make internals functions of time manager in private

--- src/TimeManager.js
+++ src/TimeManager.js
@@ -36,11 +36,18 @@ export class TimeManager extends EventTarget{
         return this;
     }
 
-    play() { //do this after pause when start is clicked
-        this.startTime += performance.now() - this.pauseTime;
-        this.running = true;
-        this.update();
-        return this;
+    start() {
+        if (!this.started) {
+            this.started = true;
+            this.running = true;
+            this.startTime = performance.now();
+            this._update();
+            return this;
+        } else if (!this.running && (this.pauseTime - this.startTime) <  this.duration) {
+            this._play();
+        } else {
+            this._restart();
+        }
     }
 
     reset() {
@@ -53,33 +60,26 @@ export class TimeManager extends EventTarget{
         this.started = false;
     }
 
-    restart() {
-        this.reset();
-        this.update();
+    _play() { //do this after pause when start is clicked
+        this.startTime += performance.now() - this.pauseTime;
+        this.running = true;
+        this._update();
         return this;
     }
 
-    start() {
-        if (!this.started) {
-            this.started = true;
-            this.running = true;
-            this.startTime = performance.now();
-            this.update();
-            return this;
-        } else if (!this.running && (this.pauseTime - this.startTime) <  this.duration) {
-            this.play();
-        } else {
-            this.restart();
-        }
+    _restart() {
+        this.reset();
+        this._update();
+        return this;
     }
 
-    update() {
+    _update() {
         if(this.remainingTime < 0 && this.running) {
             this.pause();
             this.reset();
             this.dispatchEvent(TimeoutEvent);
         } else {
-            this.req = requestAnimationFrame(this.update.bind(this));
+            this.req = requestAnimationFrame(this._update.bind(this));
         }
     }
 
@@ -92,3 +92,4 @@ export class TimeManager extends EventTarget{
     }
 
 }
+
