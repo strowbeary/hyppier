2ad1db28b28eb1471d4dadb484dfc0ec8d428891
StrowBeary <remicaillot5@gmail.com>
2018-11-28 14:33:48 +0100

    back to 3D

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import createReactClass from "create-react-class";
 import "./_app.scss";
 import {observer} from "mobx-react";
-import Notification from "./notification/Notification";
-import TimeManager from "../../utils/TimeManager";
+import ThreeContainer from "./threeContainer/ThreeContainer";
 const App = {
     displayName: "App",
     componentDidMount() {
@@ -12,20 +11,7 @@ const App = {
     render() {
         return (
             <div id="app">
-                <Notification time={5000}/>
-                <Notification time={5000}/>
-                <Notification time={5000}/>
-                <div>
-                <button onClick={() => {
-                    TimeManager.startAll();
-                }}>Start</button>
-                <button onClick={() => {
-                    TimeManager.pauseAll();
-                }}>Pause</button>
-                <button onClick={() => {
-                    TimeManager.stopAll();
-                }}>Stop</button>
-            </div>
+                <ThreeContainer/>
             </div>
         )
     }
