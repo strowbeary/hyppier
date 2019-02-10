51baf0e7023a6bf39b94a1ed61ba0d1ef3e4a37c
StrowBeary <remicaillot5@gmail.com>
2019-02-10 18:28:00 +0100

    calmez-vous

--- src/components/app/GameCanvas/Lights.js
+++ src/components/app/GameCanvas/Lights.js
@@ -31,9 +31,9 @@ export class Lights{
         lightSphere2.material = new BABYLON.StandardMaterial("light", scene);
         lightSphere2.material.emissiveColor = new BABYLON.Color3(1, 0, 1);
         */
-        this.shadowGenerator = new BABYLON.ShadowGenerator(Math.pow(2, 14), light);
+        this.shadowGenerator = new BABYLON.ShadowGenerator(Math.pow(2, 13), light);
         this.shadowGenerator.setDarkness(0.3);
-        this.shadowGenerator.useExponentialShadowMap = true;
+        this.shadowGenerator.usePoissonSampling = true;
         this.shadowGenerator.setTransparencyShadow(false);
 
     }
