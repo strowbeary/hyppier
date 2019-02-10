e6f114e3d683871883e60f7c37e45ebe5ff387a3
StrowBeary <remicaillot5@gmail.com>
2019-02-11 00:36:12 +0100

    parallax

--- src/components/app/startScreen/StartScreen.js
+++ src/components/app/startScreen/StartScreen.js
@@ -7,12 +7,30 @@ import objects from "../../../assets/img/objects.png";
 import objectsBlurred from "../../../assets/img/objects-blurred.png";
 
 const StartScreen = observer(class StartScreen extends Component {
+    state = {
+        x: 0
+    };
+    componentDidMount() {
+        this.mousemove = document.addEventListener("mousemove", (e) => {
+            this.setState({
+                x: (e.clientX / window.innerWidth - 0.5)
+            });
+        })
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("mousemove", this.mousemove);
+    }
 
     render() {
         return (
             <div className="startScreen">
-                <img src={objects} alt="decoObjects" className="decoObjects"/>
-                <img src={objectsBlurred} alt="decoObjectsBlurred" className="decoObjectsBlurred"/>
+                <img src={objects} alt="decoObjects" className="decoObjects" style={{
+                    left: `${this.state.x * 100}px`
+                }}/>
+                <img src={objectsBlurred} alt="decoObjectsBlurred" className="decoObjectsBlurred" style={{
+                    left: `${this.state.x * 200}px`
+                }}/>
                 <div className="startScreen__wrapper">
                     <div className={"startScreen__text"}>
                         <h1 className="startScreen__title">Hyppier</h1>
