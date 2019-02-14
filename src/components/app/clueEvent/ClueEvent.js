d3289567be4ab6f14ce5256ea3848a948e8474c3
mel <melanie.ngo@edu.gobelins.fr>
2019-02-14 16:29:30 +0100

    Fix clueEvent

--- src/components/app/clueEvent/ClueEvent.js
+++ src/components/app/clueEvent/ClueEvent.js
@@ -15,6 +15,7 @@ const ClueEvent = observer(class ClueEvent extends Component {
     componentDidMount() {
         if (this.type === "electric") {
             SoundManagerInstance.clueEventElectric.play();
+            SoundManagerInstance.music.pause();
         }
     }
 
@@ -23,7 +24,8 @@ const ClueEvent = observer(class ClueEvent extends Component {
     }
 
     componentWillUnmount() {
-        this.sceneManager.gameManager.playAfterClueEventClosed()
+        SoundManagerInstance.music.play();
+        this.sceneManager.gameManager.playAfterClueEventClosed();
     }
 
     render() {
@@ -33,7 +35,7 @@ const ClueEvent = observer(class ClueEvent extends Component {
                     <div className="clueEvent__wrapper">
                         <h3 className="clueEvent__title">Boom!</h3>
                         <p>Tu t’es lâché.e sur le nombre d’appareils électroniques, les plombs ont sauté…</p>
-                        <p>Retire un objet gourmand en énergie en appuyant sur espace</p>
+                        <p>Retire un objet gourmand en énergie en appuyant sur espace.</p>
                     </div>
                 }
                 <SpaceBar onSpaceUp={() => this.closeClueEvent()} color={"white"}/>
